c6b97cb8b1a1604efd98e0276eb91a2071ef841d
Juliana Viccari <juliana.z.viccari@gmail.com>
2023-04-30 00:21:24 -0300

    wip: create api

--- src/entities/product.ts
+++ src/entities/product.ts
@@ -3,12 +3,29 @@ export default class Product {
     private name: string; 
     private description: string; 
     private price: number;
+    private width?: number;
+    private height?: number;
+    private length?: number;
+    private weight?: number;
 
-    constructor(id: string, name: string, description: string, price: number){
+    constructor(
+        id: string,
+        name: string, 
+        description: string, 
+        price: number,
+        width?: number,
+        height?: number,
+        length?: number,
+        weight?: number,
+        ){
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
+        this.width = width;
+        this.height = height;
+        this.length = length;
+        this.weight = weight;
     }
 
     getId() :string {
@@ -25,5 +42,21 @@ export default class Product {
 
     getPrice():number {
         return this.price;
+    }
+
+    getWidth() :number {
+        return this.width === undefined ? 0 : this.width;
+    }
+
+    getHeight() :number {
+        return this.height === undefined ? 0 : this.height;
+    }
+
+    getLength() :number {
+        return this.length === undefined ? 0 : this.length;
+    }
+
+    getWeight() :number {
+        return this.weight === undefined ? 0 : this.weight;
     }
 } 
--- test/api_rest.test.ts
+++ test/api_rest.test.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 import Product from "../src/entities/product";
 import OrderDetail from "../src/entities/order_detail";
-import Coupon from "../src/entities/coupon";
+
+axios.defaults.validateStatus = function (){
+    return true;
+};
 
 test('must not create an order with invalid CPF', async function () {
     const input = {
@@ -32,7 +35,7 @@ test('should make an order with three items', async function () {
    expect(output.total).toBe(148);
 });
 
-test('should make an order with three items whith quantity negative', async function () {
+test('should make an order with three items with quantity negative', async function () {
     let products = [
         new Product("1", "Dove", "shampoo", 17.00),
         new Product("2", "Siege", "shampoo", 48.00),
@@ -52,28 +55,27 @@ test('should make an order with three items whith quantity negative', async func
    expect(output.message).toBe("Product quantity cannot be negative");
 });
 
-// test('must not repeat item in input', async function () {
-//     let products = [
-//         new Product("1", "Dove", "shampoo", 17.00),
-//         new Product("2", "Siege", "shampoo", 48.00),
-//         new Product("3", "Dove", "condicionador", 22.00),
-//     ];
-//     let listOrderDetails = [
-//         new OrderDetail(products[0], 2), 
-//         new OrderDetail(products[1], 1),
-//         new OrderDetail(products[0], 1),
-//     ]
-//     const input = {
-//        cpf: "407.302.170-27",
-//        items: listOrderDetails
-//     };
-//    const response = await axios.post("http://localhost:3000/checkout", input)
-//    const output = response.data;
-//    expect(output.message).toBe("must not repeat item");
-// });
+test('must not repeat item in input', async function () {
+    let products = [
+        new Product("1", "Dove", "shampoo", 17.00),
+        new Product("2", "Siege", "shampoo", 48.00),
+        new Product("3", "Dove", "condicionador", 22.00),
+    ];
+    let listOrderDetails = [
+        new OrderDetail(products[0], 2), 
+        new OrderDetail(products[1], 1),
+        new OrderDetail(products[0], 1),
+    ]
+    const input = {
+       cpf: "407.302.170-27",
+       items: listOrderDetails
+    };
+   const response = await axios.post("http://localhost:3000/checkout", input)
+   const output = response.data;
+   expect(output.message).toBe("must not repeat item");
+});
 
-
-test('should make an order with three items whith coupon', async function () {
+test('should make an order with three items with coupon', async function () {
     let products = [
         new Product("1", "Dove", "shampoo", 17.00),
         new Product("2", "Siege", "shampoo", 48.00),
@@ -87,14 +89,14 @@ test('should make an order with three items whith coupon', async function () {
     const input = {
         cpf: "407.302.170-27",
         items: listOrderDetails,
-        coupon: "discount10",
+        coupon: "discount20",
     };
     const response = await axios.post("http://localhost:3000/checkout", input)
     const output = response.data;
-    expect(output.total).toBe(133.2);
+    expect(output.total).toBe(118.4);
 });
 
-test('should make an order with three items whith coupon invalid', async function () {
+test('should make an order with three items with coupon invalid', async function () {
     let products = [
         new Product("1", "Dove", "shampoo", 17.00),
         new Product("2", "Siege", "shampoo", 48.00),
@@ -109,9 +111,54 @@ test('should make an order with three items whith coupon invalid', async functio
     const input = {
        cpf: "407.302.170-27",
        items: listOrderDetails,
-       coupon: "discount5",
+       coupon: "discount",
     };
    const response = await axios.post("http://localhost:3000/checkout", input)
    const output = response.data;
    expect(output.message).toBe("Coupon invalid");
 });
+
+test('should make an order with three items with shipment', async function () {
+    let products = [
+        new Product("1", "Dove", "shampoo", 17.00),
+        new Product("2", "Siege", "shampoo", 48.00),
+    ];
+    let listOrderDetails = [
+        new OrderDetail(products[0], 2), 
+        new OrderDetail(products[1], 1),
+    ];
+
+    const input = {
+       cpf: "407.302.170-27",
+       items: listOrderDetails,
+       from: "14620000", //cep origem
+       to: "06445550", //cep destino
+    };
+   const response = await axios.post("http://localhost:3000/checkout", input)
+   const output = response.data;
+   expect(output.freight).toBe(6010);
+   expect(output.subtotal).toBe(82);
+   expect(output.total).toBe(6092);
+});
+
+test('should not create order if product\'s dimensions has negative values', async function () {
+    let products = [
+        new Product("1", "Dove", "shampoo", 17.00),
+        new Product("4", "Lux", "sabonete", 2.00),
+    ];
+    let listOrderDetails = [
+        new OrderDetail(products[0], 2), 
+        new OrderDetail(products[1], 1),
+    ];
+
+    const input = {
+       cpf: "407.302.170-27",
+       items: listOrderDetails,
+       from: "14620000", //cep origem
+       to: "06445550", //cep destino
+    };
+   const response = await axios.post("http://localhost:3000/checkout", input)
+   const output = response.data;
+   expect(output.message).toBe("Invalid dimensions");
+});
+
