66cf6422fe91efb6dff4b3b573d26bbf5306338c
Juliana Viccari <juliana.z.viccari@gmail.com>
2023-06-11 03:53:11 -0300

    add pm2

---
+++ pm2/ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [
+    {
+      name: "auth",
+      script: "npx ts-node ../auth/src/main_api.ts"
+    },
+    {
+      name: "checkout",
+      script: "npx ts-node ../checkout/src/main_api.ts"
+    },
+    {
+      name: "catalog",
+      script: "npx ts-node ../catalog/src/main_api.ts"
+    },
+    {
+      name: "freight",
+      script: "npx ts-node ../freight/src/main_api.ts"
+    }
+  ],
+
+  
+};
